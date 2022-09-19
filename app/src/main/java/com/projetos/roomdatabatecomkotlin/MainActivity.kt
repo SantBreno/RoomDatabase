529103c16469a4bc6cab4c125847a569425956bd
SantBreno <brenosantsousa4@outlook.com>
2022-09-19 20:48:56 -0300

    first commit of the project

---
+++ app/src/main/java/com/projetos/roomdatabatecomkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.projetos.roomdatabatecomkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
